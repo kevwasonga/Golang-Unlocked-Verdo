5958901246f400389ec1fa5bc6b46ee4d6ae4357
kevwasonga <kevinwasonga116@gmail.com>
2024-06-19 21:53:14 +0300

    COMPARE:This function ensures that the strings are compared lexicographically and handles cases where one string might be a prefix of another by checking their lengths after character-by-character comparison.

---
+++ compare/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(Compare("Hello!", "Hello!"))
+	fmt.Println(Compare("Salut!", "lut!"))
+	fmt.Println(Compare("Ola!", "Ol"))
+}
+
+func Compare(a, b string) int {
+
+	for i := 0; i < len(a) && i < len(b); i++ {
+		if a[i] < b[i] {
+			return -1
+		} else if a[i] > b[i] {
+			return 1
+		}
+	}
+
+	if len(a) < len(b) {
+		return -1
+	} else if len(a) > len(b) {
+		return 1
+	}
+	return 0
+
+}
