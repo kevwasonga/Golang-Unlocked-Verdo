60d96661f7e078410312d930677053e26a2a5322
kevwasonga <kevinwasonga116@gmail.com>
2024-09-20 23:52:45 +0300

    convert roman numbers to integers

---
+++ level5/romannumbers/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := "XXII"
+
+	fmt.Println(romans(input))
+}
+
+func romans(s string) int {
+	maps := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	previous := 0
+
+	var total int
+
+	for i := len(s) - 1; i >= 0; i-- {
+		current := maps[s[i]]
+
+		if previous > current {
+			total -= current
+		} else {
+			total += current
+		}
+
+	}
+
+	return total
+}
