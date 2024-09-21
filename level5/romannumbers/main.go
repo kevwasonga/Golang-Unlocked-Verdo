f0b0efbf694f5821176333cab6136d608df2bc7d
kevwasonga <kevinwasonga116@gmail.com>
2024-09-21 04:25:42 +0300

    convert roman numbers to integers

--- level5/romannumbers/main.go
+++ level5/romannumbers/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	input := "MCMXCIV" //"XXII"
+	input := "MCMXCIV" // Test input for Roman numeral
 
-	fmt.Println(romans(input))
+	fmt.Println(romans(input)) // Convert and print the result
 }
 
+// Function to convert Roman numeral string to integer
 func romans(s string) int {
+	// Map of Roman numeral characters to their integer values
 	maps := map[byte]int{
 		'I': 1,
 		'V': 5,
@@ -19,22 +21,23 @@ func romans(s string) int {
 		'M': 1000,
 	}
 
-	previous := 0
+	previous := 0 // Variable to hold the previous numeral value
+	var total int // Variable to accumulate the total value
 
-	var total int
-
+	// Iterate through the string from the last character to the first
 	for i := len(s) - 1; i >= 0; i-- {
-		current := maps[s[i]]
+		current := maps[s[i]] // Get the current numeral value
 
+		// If the previous numeral is greater, we subtract the current value
 		if previous > current {
 			total -= current
 		} else {
+			// Otherwise, we add the current value
 			total += current
 		}
-		//update the last processed to be the new "previous"
+		// Update the previous value to the current
 		previous = current
-
 	}
 
-	return total
+	return total // Return the final calculated total
 }
